41ca79de0ff3e95834198d1c6186034175d6dd25
vctrhugoop <vhdestroy@hotmail.com>
2024-02-06 22:07:13 -0300

    🌈 style: add responsive layout

--- src/app.tsx
+++ src/app.tsx
@@ -4,17 +4,17 @@ import { NoteCard } from "./components/note-card";
 
 export function App() {
   return (
-    <div className="mx-auto my-12 max-w-6xl space-y-6">
+    <div className="mx-auto my-12 max-w-6xl space-y-6 px-6 ">
       <img src={logo} alt="Logo NLW Expert" />
       <form className="w-full border-b border-slate-700 pb-6">
         <input
           type="text"
           placeholder="Busque em suas notas..."
-          className="w-full bg-transparent text-3xl font-semibold tracking-tight outline-none placeholder:text-slate-500"
+          className="w-full bg-transparent text-xl font-semibold tracking-tight outline-none placeholder:text-slate-500 md:text-3xl"
         />
       </form>
 
-      <div className="grid auto-rows-[250px] grid-cols-3 gap-6 ">
+      <div className="grid auto-rows-[250px] gap-6 md:grid-cols-3">
         <NewNoteCard />
         <NoteCard
           note={{
