64f9ce692d92131af96193d4d0518db0ec45a7a9
vctrhugoop <vhdestroy@hotmail.com>
2024-02-07 21:02:02 -0300

    ✨ feat: add seave note localstorege and search notes

--- src/app.tsx
+++ src/app.tsx
@@ -1,8 +1,53 @@
+import { ChangeEvent, useState } from "react";
 import logo from "./assets/logo-nlw.svg";
 import { NewNoteCard } from "./components/new-note-card";
 import { NoteCard } from "./components/note-card";
 
+interface Note {
+  id: string;
+  date: Date;
+  content: string;
+}
+
 export function App() {
+  const [search, setSearch] = useState("");
+  const [notes, setnotes] = useState<Note[]>(() => {
+    const noteOnStorege = localStorage.getItem("notes");
+
+    if (noteOnStorege) {
+      return JSON.parse(noteOnStorege);
+    }
+
+    return [];
+  });
+
+  function onNoteCreated(content: string) {
+    const newNote = {
+      id: crypto.randomUUID(),
+      date: new Date(),
+      content,
+    };
+
+    const notesArray = [newNote, ...notes];
+
+    setnotes([newNote, ...notes]);
+
+    localStorage.setItem("notes", JSON.stringify(notesArray));
+  }
+
+  function handleSearch(event: ChangeEvent<HTMLInputElement>) {
+    const query = event.target.value;
+
+    setSearch(query);
+  }
+
+  const filteredNotes =
+    search !== ""
+      ? notes.filter((note) =>
+          note.content.toLocaleLowerCase().includes(search.toLocaleLowerCase()),
+        )
+      : notes;
+
   return (
     <div className="mx-auto my-12 max-w-6xl space-y-6 px-6 ">
       <img src={logo} alt="Logo NLW Expert" />
@@ -10,18 +55,16 @@ export function App() {
         <input
           type="text"
           placeholder="Busque em suas notas..."
+          onChange={handleSearch}
           className="w-full bg-transparent text-xl font-semibold tracking-tight outline-none placeholder:text-slate-500 md:text-3xl"
         />
       </form>
 
       <div className="grid auto-rows-[250px] gap-6 md:grid-cols-3">
-        <NewNoteCard />
-        <NoteCard
-          note={{
-            date: new Date(),
-            content: "Hello World",
-          }}
-        />
+        <NewNoteCard onNoteCreated={onNoteCreated} />
+        {filteredNotes.map((note) => {
+          return <NoteCard key={note.id} note={note} />;
+        })}
       </div>
     </div>
   );
--- src/components/new-note-card.tsx
+++ src/components/new-note-card.tsx
@@ -3,7 +3,11 @@ import { X } from "lucide-react";
 import { ChangeEvent, FormEvent, useState } from "react";
 import { toast } from "sonner";
 
-export function NewNoteCard() {
+interface NewNoteCardProps {
+  onNoteCreated: (content: string) => void;
+}
+
+export function NewNoteCard({ onNoteCreated }: NewNoteCardProps) {
   const [shouldShowOnboarding, setShouldShowOnboarding] = useState(true);
   const [content, setContent] = useState("");
 
@@ -21,6 +25,12 @@ export function NewNoteCard() {
 
   function handleSaveNote(event: FormEvent) {
     event.preventDefault();
+
+    onNoteCreated(content);
+
+    setContent("");
+
+    setShouldShowOnboarding(true);
 
     toast.success("Nota criada com sucesso!");
   }
@@ -64,6 +74,7 @@ export function NewNoteCard() {
                 </p>
               ) : (
                 <textarea
+                  value={content}
                   autoFocus
                   className="flex-1 resize-none bg-transparent text-sm leading-6 text-slate-400 outline-none"
                   onChange={handleContentChanged}
